ace4a96ebd1c2c70957f80a91383fc36b09b1450
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2024-01-28 17:06:13 -0300

    feat: html create

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { FiSearch } from 'react-icons/fi';
+
 
 function App() {
   return (
-    <div className="App">
-      Teste projeto
+
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      <div className="container-input">
+        <input type="text" name="entrada" placeholder="Digite seu CEP..."/>
+    
+        <button className="buttonSearch">
+          <FiSearch size={25} color='#FFF'/>
+        </button>
+      </div>
+
+      <main className='container-main'>
+        <h2>Cep: 15486458</h2>
+
+        <span>Rua teste</span>
+        <span>Complemento: Algum lugar</span>
+        <span>Vila rosa</span>
+        <span>Rio Grande do Norte</span>
+      </main>
+      
     </div>
   );
 }
